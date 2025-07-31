48488bc823bd9028d9c9a9fd227b1684a1f589bc
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-31 16:39:14 +0200

    adjust code to review

--- components/StyledLink.jsx
+++ components/StyledLink.jsx
@@ -1,33 +1,10 @@
 import styled from "styled-components";
 import Link from "next/link";
 
-// export const StyledLink = styled(Link)`
-//   font-family: var(--font-family-body);
-//   display: inline-block;
-//   justify-self: center;
-//   border: 1px solid black;
-//   margin-top: 0.75rem;
-//   background: transparent;
-//   padding: 0.5em 1em;
-//   font-size: 1rem;
-//   border-radius: 0.12rem;
-//   width: auto;
-//   cursor: pointer;
-//   font-weight: 400;
-//   color: var(--color-light-dark);
-//   text-decoration: none;
-//   text-align: center;
-
-//   &:hover {
-//     background: var(--color-light-dark);
-//     color: var(--color-natural-white);
-//   }
-// `;
-
-export const StyledLink = styled(Link)`
+export const StyledAnchor = styled.a`
   font-family: var(--font-family-body);
   display: block; /* wichtig für margin auto */
-  margin: 1rem auto 0; /* zentriert, mit etwas Abstand oben */
+  /* margin: 1rem auto 0; zentriert, mit etwas Abstand oben */
   border: 1px solid black;
   background: transparent;
   padding: 0.5em 1em;
@@ -45,3 +22,10 @@ export const StyledLink = styled(Link)`
     color: var(--color-natural-white);
   }
 `;
+export function StyledLink({ href, children }) {
+  return (
+    <Link href={href} passHref legacyBehavior>
+      <StyledAnchor>{children}</StyledAnchor>
+    </Link>
+  );
+}
