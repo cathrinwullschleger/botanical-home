ff4f1af32827e2c426dc6e989d54671accd8340a
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-08-13 11:12:16 +0200

    parameter Changed

--- db/connect.js
+++ db/connect.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
--- pages/api/plants/[slug]/index.js
+++ pages/api/plants/[slug]/index.js
@@ -4,9 +4,9 @@ import Plant from "@/db/models/Plant.js";
 export default async function handler(request, response) {
   try {
     await dbConnect();
-    const { id } = request.query;
+    const { slug } = request.query;
     if (request.method === "GET") {
-      const plant = await Plant.findById(id);
+      const plant = await Plant.findById(slug);
 
       if (!plant) {
         return response.status(404).json({ status: "No Plant found!" });
@@ -15,24 +15,24 @@ export default async function handler(request, response) {
     }
 
     if (request.method === "DELETE") {
-      const deleted = await Plant.findByIdAndDelete(id);
+      const deleted = await Plant.findByIdAndDelete(slug);
       if (!deleted) {
         return response.status(404).json({ status: "No Plant found!" });
       }
       return response
         .status(200)
-        .json({ status: `Plant${id} is successfully deleted.` });
+        .json({ status: `Plant${slug} is successfully deleted.` });
     }
 
     if (request.method === "PUT") {
       const plantData = request.body;
-      const updated = await Plant.findByIdAndUpdate(id, plantData);
+      const updated = await Plant.findByIdAndUpdate(slug, plantData);
       if (!updated) {
         return response.status(404).json({ status: "No Plant found!" });
       }
       return response
         .status(200)
-        .json({ status: `Plant${id} is successfully updated.` });
+        .json({ status: `Plant${slug} is successfully updated.` });
     }
     return response.status(405).json({ status: "Method not allowed!" });
   } catch (error) {
